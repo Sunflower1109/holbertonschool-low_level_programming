76c3467e87faaf070e6460c43656ca1c3923032e
Sunflower1109 <12097@holbertonstudents.com>
2025-10-16 09:34:21 +0000

    Add 5-printf.c to print with printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific string using printf and returns 0
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
