3474e083b3c2c993df0f9b9c3904f52556b3b0c4
Sunflower1109 <12097@holbertonstudents.com>
2025-10-18 04:05:14 +0000

    Add 8-print_base16: Print hex 0-9 + a-f (2 loops)

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	 int digit;
+
+	 for (digit = 0; digit < 10; digit++)
+		 putchar(digit + '0');
+
+	 for (digit = 0; digit < 6; digit++)
+	 	putchar(digit + 'a');
+
+		 putchar('\n');
+	 return 0;
+}
