336aa924b9886f8a8a2155151cf84040c766864d
Sunflower1109 <12097@holbertonstudents.com>
2025-10-18 00:54:26 +0000

    updated description for 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,12 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Checks if a random number is positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
