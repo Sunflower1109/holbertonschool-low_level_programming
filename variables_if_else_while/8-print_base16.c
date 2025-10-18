84bdc7cb7839b1305a8fb4907525271f4dce5bce
Sunflower1109 <12097@holbertonstudents.com>
2025-10-18 04:08:14 +0000

    Add 8-print_base16: Print hex 0-9 + a-f (2 loops)

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -5,17 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	 int digit;
+	int digit;
 
-	 for (digit = 0; digit < 10; digit++)
-		 putchar(digit + '0');
+	for (digit = 0; digit < 10; digit++)
+		putchar(digit + '0');
 
-	 for (digit = 0; digit < 6; digit++)
-	 	putchar(digit + 'a');
+	for (digit = 0; digit < 6; digit++)
+		putchar(digit + 'a');
 
-		 putchar('\n');
-	 return 0;
+	putchar('\n');
+	return (0);
 }
