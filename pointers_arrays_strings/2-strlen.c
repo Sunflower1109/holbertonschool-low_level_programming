14ff4455409065b9e5f1cbbab117b90805e90fc0
Sunflower1109 <12097@holbertonstudents.com>
2025-10-22 07:46:09 +0000

    updates to  _strlen - returns the length of a string

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strlen - returns the length of a string
@@ -9,5 +8,10 @@
 
 int _strlen(char *s)
 {
-	return (strlen(s));
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	return (length);
 }
