84dac24d5a4e94d467cf6c15be08f66ccebce953
Sunflower1109 <12097@holbertonstudents.com>
2025-11-03 03:57:28 +0000

    adds positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -7,19 +7,19 @@
  * @s: string to check
  * Return: 1 if string is a valid number, 0 otherwise
  */
-int is_number(char *s)
+int	is_number(char *s)
 {
-    int i = 0;
+	int	i = 0;
 
-    if (!s)
-        return 0;
-    while (s[i])
-    {
-        if (!isdigit(s[i]))
-            return 0;
-        i++;
-    }
-    return 1;
+	if (!s)
+		return (0);
+	while (s[i])
+	{
+		if (!isdigit(s[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
@@ -28,27 +28,27 @@ int is_number(char *s)
  * @argv: array of command line arguments
  * Return: 0 on success, 1 on error
  */
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int sum = 0, i, num;
+	int	sum = 0, i, num;
 
-    if (argc == 1)
-    {
-        printf("0\n");
-        return 0;
-    }
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    for (i = 1; i < argc; i++)
-    {
-        if (!is_number(argv[i]))
-        {
-            printf("Error\n");
-            return 1;
-        }
-        num = atoi(argv[i]);
-        sum += num;
-    }
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		num = atoi(argv[i]);
+		sum += num;
+	}
 
-    printf("%d\n", sum);
-    return 0;
+	printf("%d\n", sum);
+	return (0);
 }
