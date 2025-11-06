49695ee933bdd7290fc937235e525a4f51b336ad
Sunflower1109 <12097@holbertonstudents.com>
2025-11-06 02:08:43 +0000

    function allocates memory for an array of nmemb elements of size bytes

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _calloc - function allocates memory for an array.
+* @nmemb: array of elements.
+* @size: size of element of array in bytes.
+* Return: pointer to the allocated memory,
+* if nmemb or size is 0 - return NULL,if malloc fails - return 0.
+**/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+	ptr = malloc(nmemb * size);
+
+	for (i = 0; i < (nmemb * size); i++)
+	ptr[i] = 0;
+
+	return (ptr);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -1,3 +1,3 @@
 0. 0-malloc_checked.c - function that allocates memory using malloc.
-1.1- string_nconcat - concatenates s1 with first n bytes of s2.
-
+1. 1- string_nconcat - concatenates s1 with first n bytes of s2.
+3. 2-calloc.c - function allocates memory for an array elements of bytes.
